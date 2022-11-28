e84b5e5d11722af562aa6ee8ed13b0057e985a7f
Črt Goričan <crt.gorican@ssrd.io>
2022-11-28 15:43:38 +0100

    Fragments init

--- app/src/main/java/si/um/feri/hillclimbracing/MainActivity.kt
+++ app/src/main/java/si/um/feri/hillclimbracing/MainActivity.kt
@@ -2,10 +2,24 @@ package si.um.feri.hillclimbracing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import si.um.feri.hillclimbracing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val googleMaps = MapsFragment()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, googleMaps)
+            //.add(R.id.fragmentContainerView, inputFragment)
+            .commit()
     }
 }
